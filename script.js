f23aa0a8dc14bb56632f64937a0a289e5faccfd6
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-04-24 09:37:53 -0500

    refactored to use module pattern. Working

--- script.js
+++ script.js
@@ -1,79 +1,83 @@
-// Constructor
-function Book(title, author, pages, readStatus) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readStatus = readStatus;
-}
+const library = (function () {
+  // Bookshelf
+  const books = [];
 
-const library = {
-  books: [],
-  init() {
-    this.cacheDom();
-    this.bindEventsStatic();
-  },
-  cacheDom() {
-    this.showModal = document.querySelector('.show-modal');
-    this.form = document.querySelector('.form');
-    this.modal = document.querySelector('.modal');
-    this.submit = document.querySelector('.submit');
-    this.cancel = document.querySelector('.cancel');
-    this.template = document.querySelector('.template').innerHTML;
-    this.booksContainer = document.querySelector('.book-cards-container');
-    this.formTitle = document.getElementById('title');
-    this.formAuthor = document.getElementById('author');
-    this.formPages = document.getElementById('pages');
-    this.formReadStatus = document.getElementById('readStatus');
-  },
-  bindEventsStatic() {
-    this.showModal.addEventListener('click', () => {
-      this.modal.showModal();
-    });
-    this.submit.addEventListener('click', () => {
-      this.modal.close();
-    });
-    this.cancel.addEventListener('click', () => {
-      this.modal.close();
-      this.form.reset();
-    });
-    this.form.addEventListener('submit', this.addToLibrary.bind(this));
-  },
-  bindEventsDynamic() {
-    this.booksContainer.querySelectorAll('.remove').forEach((btn) => {
-      btn.addEventListener('click', this.removeFromLibrary.bind(this));
-    });
-  },
-  render() {
-    const finalMarkup = this.books
+  // cacheDom
+  const showModal = document.querySelector('.show-modal');
+  const form = document.querySelector('.form');
+  const modal = document.querySelector('.modal');
+  const submit = document.querySelector('.submit');
+  const cancel = document.querySelector('.cancel');
+  const template = document.querySelector('.template').innerHTML;
+  const booksContainer = document.querySelector('.book-cards-container');
+  const formTitle = document.getElementById('title');
+  const formAuthor = document.getElementById('author');
+  const formPages = document.getElementById('pages');
+  const formReadStatus = document.getElementById('readStatus');
+
+  // Book Constructor
+
+  function Book(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+  }
+
+  // Methods
+  const render = () => {
+    const finalMarkup = books
       .map(({
         title, author, pages, readStatus,
-      }) => this.template
+      }) => template
         .replaceAll('{title}', title)
         .replace('{author}', author)
         .replace('{pages}', pages)
         .replace('{readStatus}', readStatus));
-    this.booksContainer.innerHTML = finalMarkup;
-    this.bindEventsDynamic();
-  },
-  addToLibrary(e) {
+    booksContainer.innerHTML = finalMarkup;
+    bindEventsDynamic();
+  };
+
+  const addToLibrary = (e) => {
     e.preventDefault();
     const book = new Book(
-      this.formTitle.value,
-      this.formAuthor.value,
-      this.formPages.value,
-      this.formReadStatus.checked,
+      formTitle.value,
+      formAuthor.value,
+      formPages.value,
+      formReadStatus.checked,
     );
-    this.books.push(book);
-    this.render();
-    this.modal.close();
-    this.form.reset();
-  },
-  removeFromLibrary(e) {
-    const target = e.target.parentNode.dataset.title;
-    const index = this.books.findIndex((x) => x.title === target);
-    this.books.splice(index, 1);
-    this.render();
-  },
-};
+    books.push(book);
+    render();
+    modal.close();
+    form.reset();
+  };
 
-library.init();
+  const removeFromLibrary = (e) => {
+    const target = e.target.parentNode.dataset.title;
+    const index = books.findIndex((x) => x.title === target);
+    books.splice(index, 1);
+    render();
+  };
+
+  // bindEventsStatic
+  showModal.addEventListener('click', () => {
+    modal.showModal();
+  });
+  submit.addEventListener('click', () => {
+    modal.close();
+  });
+  cancel.addEventListener('click', () => {
+    modal.close();
+    form.reset();
+  });
+  form.addEventListener('submit', addToLibrary);
+
+  const bindEventsDynamic = () => {
+    booksContainer.querySelectorAll('.remove').forEach((btn) => {
+      btn.addEventListener('click', removeFromLibrary);
+    });
+  };
+  return {
+    books,
+  };
+}());
