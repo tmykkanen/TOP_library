ca1c3b3127c26cb2fb82ce542ae7174e148a6e42
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-04-24 06:28:18 -0500

    function broken. opps

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const myLibrary = [];
-
-const form = document.querySelector('.form');
-const dialog = document.querySelector('dialog');
-const showButton = document.querySelector('.show-modal');
-const submitButton = document.querySelector('.submit');
-const closeButton = document.querySelector('.close');
-
-const template = document.querySelector('.template');
-
-console.log(template.innerHTML);
-
-
 // Constructor
 function Book(title, author, pages, readStatus) {
   this.title = title;
@@ -19,66 +6,169 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus;
 }
 
-// Event Listeners
-showButton.addEventListener('click', () => {
-  dialog.showModal();
-});
-
-submitButton.addEventListener('click', () => {
-  dialog.close();
-});
-
-closeButton.addEventListener('click', () => {
-  dialog.close();
-  form.reset();
-});
-
-function addBookToLibrary(...args) {
-  const book = new Book(...args);
-  myLibrary.push(book);
-}
-
-function displayBooks(obj) {
-  const htmlTemplate = template.innerHTML;
-
-  const targetNode = document.querySelector('.book-cards-container');
-  const finalMarkup = obj
-    .map(({
-      title, author, pages, readStatus,
-    }) => htmlTemplate
-      .replaceAll('{title}', title)
-      .replace('{author}', author)
-      .replace('{pages}', pages)
-      .replace('{readStatus}', readStatus));
-  targetNode.innerHTML = finalMarkup;
-  initRemoveButtons();
-}
-
-function removeBookFromLibrary(title) {
-  console.log(myLibrary);
-  const target = myLibrary.indexOf(title);
-  myLibrary.splice(target, 1);
-  console.log(target);
-  displayBooks(myLibrary);
-}
-
-function initRemoveButtons() {
-  console.log('did it run?');
-  document.querySelectorAll('.remove').forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      console.log(e.target.parentNode.dataset.title);
-      removeBookFromLibrary(e.target.parentNode.dataset.title);
+const library = {
+  books: [],
+  init() {
+    this.cacheDom();
+    this.bindEvents();
+  },
+  cacheDom() {
+    this.showModal = document.querySelector('.show-modal');
+    this.form = document.querySelector('.form');
+    this.modal = document.querySelector('.modal');
+    this.submit = document.querySelector('.submit');
+    this.cancel = document.querySelector('.cancel');
+    this.template = document.querySelector('.template').innerHTML;
+    this.booksContainer = document.querySelector('.book-cards-container');
+    this.formTitle = document.getElementById('title');
+    this.formAuthor = document.getElementById('author');
+    this.formPages = document.getElementById('pages');
+    this.formReadStatus = document.getElementById('readStatus');
+  },
+  bindEvents() {
+    this.showModal.addEventListener('click', () => {
+      this.modal.showModal();
     });
-  });
-}
+    this.submit.addEventListener('click', () => {
+      this.modal.close();
+    });
+    this.cancel.addEventListener('click', () => {
+      this.modal.close();
+      this.form.reset();
+    });
+    this.form.addEventListener('submit', this.processForm.bind(this));
+  },
+  render() {
+    const finalMarkup = this.books
+      .map(({
+        title, author, pages, readStatus,
+      }) => this.template
+        .replaceAll('{title}', title)
+        .replace('{author}', author)
+        .replace('{pages}', pages)
+        .replace('{readStatus}', readStatus));
+    this.booksContainer.innerHTML = finalMarkup;
+    this.bindEvents();
+  },
+  processForm() {
+    // e.preventDefault();
+    console.log(this);
+    // console.log(e);
+    const book = new Book(
+      this.formTitle.value,
+      this.formAuthor.value,
+      this.formPages.value,
+      this.formReadStatus.value,
+    );
+    console.log(book);
+    this.books.push(book);
+    console.log(this.books);
+    this.render();
+    this.modal.close();
+    this.form.reset();
+  },
+};
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const readStatus = document.getElementById('readStatus').value;
-  addBookToLibrary(title, author, pages, readStatus);
-  displayBooks(myLibrary);
-  form.reset();
-});
+library.init();
+
+// function removeBookFromLibrary(title) {
+//   console.log(myLibrary);
+//   const target = myLibrary.indexOf(title);
+//   myLibrary.splice(target, 1);
+//   console.log(target);
+//   displayBooks(myLibrary);
+// }
+
+// function initRemoveButtons() {
+//   console.log('did it run?');
+//   document.querySelectorAll('.remove').forEach((btn) => {
+//     btn.addEventListener('click', (e) => {
+//       console.log(e.target.parentNode.dataset.title);
+//       removeBookFromLibrary(e.target.parentNode.dataset.title);
+//     });
+//   });
+// }
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const title = document.getElementById('title').value;
+//   const author = document.getElementById('author').value;
+//   const pages = document.getElementById('pages').value;
+//   const readStatus = document.getElementById('readStatus').value;
+//   addBookToLibrary(title, author, pages, readStatus);
+//   displayBooks(myLibrary);
+//   form.reset();
+// });
+// Event Listeners
+// showButton.addEventListener('click', () => {
+//   dialog.showModal();
+// });
+
+// submitButton.addEventListener('click', () => {
+//   dialog.close();
+// });
+
+// closeButton.addEventListener('click', () => {
+//   dialog.close();
+//   form.reset();
+// });
+
+// function displayBooks(obj) {
+//   const htmlTemplate = template.innerHTML;
+
+//   const targetNode = document.querySelector('.book-cards-container');
+//   const finalMarkup = obj
+//     .map(({
+//       title, author, pages, readStatus,
+//     }) => htmlTemplate
+//       .replaceAll('{title}', title)
+//       .replace('{author}', author)
+//       .replace('{pages}', pages)
+//       .replace('{readStatus}', readStatus));
+//   targetNode.innerHTML = finalMarkup;
+//   initRemoveButtons();
+// }
+
+// const myLibrary = [];
+
+// const form = document.querySelector('.form');
+// const dialog = document.querySelector('dialog');
+// const showButton = document.querySelector('.show-modal');
+// const submitButton = document.querySelector('.submit');
+// const closeButton = document.querySelector('.close');
+
+// const template = document.querySelector('.template');
+
+// console.log(template.innerHTML);
+
+// function addBookToLibrary(...args) {
+//   const book = new Book(...args);
+//   myLibrary.push(book);
+// }
+
+// function displayBooks(obj) {
+//   const htmlTemplate = template.innerHTML;
+
+//   const targetNode = document.querySelector('.book-cards-container');
+//   const finalMarkup = obj
+//     .map(({
+//       title, author, pages, readStatus,
+//     }) => htmlTemplate
+//       .replaceAll('{title}', title)
+//       .replace('{author}', author)
+//       .replace('{pages}', pages)
+//       .replace('{readStatus}', readStatus));
+//   targetNode.innerHTML = finalMarkup;
+//   initRemoveButtons();
+// }
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const title = document.getElementById('title').value;
+//   const author = document.getElementById('author').value;
+//   const pages = document.getElementById('pages').value;
+//   const readStatus = document.getElementById('readStatus').value;
+//   addBookToLibrary(title, author, pages, readStatus);
+//   displayBooks(myLibrary);
+//   form.reset();
+// });
