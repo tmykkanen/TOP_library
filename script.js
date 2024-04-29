30675dd88c3eff4bd2763fd6df6f5eaf2e064421
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-04-29 16:56:23 -0500

    refactored js to class

--- script.js
+++ script.js
@@ -1,7 +1,67 @@
 /* eslint-disable arrow-body-style */
-const library = (function () {
-  // Bookshelf
-  const books = [];
+
+class Book {
+  constructor(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const library = (function library() {
+  const { render } = display;
+
+  // bookshelf array
+  const bookshelf = [];
+
+  // add book function
+  const addBookToShelf = (e) => {
+    e.preventDefault();
+    const book = new Book(
+      // TODO: MOVE TO DISPLAY AND PASS VARIABLES TO FUNCTION
+      formTitle.value,
+      formAuthor.value,
+      formPages.value,
+      formReadStatus.checked,
+    );
+    bookshelf.push(book);
+    render();
+    modal.close();
+    form.reset();
+  };
+
+  // remove book function
+  const removeBookFromShelf = (e) => {
+    const target = e.target.parentNode.dataset.title;
+    const index = bookshelf.findIndex((x) => x.title === target);
+    bookshelf.splice(index, 1);
+    render();
+  };
+
+  const toggleReadStatus = (e) => {
+    const target = e.target.parentNode.parentNode.parentNode.dataset.title;
+    const index = bookshelf.findIndex((x) => x.title === target);
+    bookshelf[index].readStatus = !bookshelf[index].readStatus;
+  };
+
+  return {
+    addBookToShelf,
+    removeBookFromShelf,
+    toggleReadStatus,
+    bookshelf,
+  };
+}());
+
+// eslint-disable-next-line no-unused-vars
+const display = (function display() {
+  const {
+    addBookToShelf,
+    removeBookFromShelf,
+    toggleReadStatus,
+    bookshelf,
+  } = library;
 
   // cacheDom
   const showModal = document.querySelector('.show-modal');
@@ -16,59 +76,6 @@ const library = (function () {
   const formPages = document.getElementById('pages');
   const formReadStatus = document.getElementById('readStatus');
 
-  // Book Constructor
-
-  function Book(title, author, pages, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readStatus = readStatus;
-  }
-
-  // Methods
-  const render = () => {
-    const finalMarkupArray = books
-      .map(({
-        title, author, pages, readStatus,
-      }) => template
-        .replaceAll('{title}', title)
-        .replace('{author}', author)
-        .replace('{pages}', pages)
-        .replace('{readStatus}', () => {
-          return readStatus === true ? 'checked' : '';
-        }));
-    const finalMarkup = finalMarkupArray.join('');
-    booksContainer.innerHTML = finalMarkup;
-    bindEventsDynamic();
-  };
-
-  const addToLibrary = (e) => {
-    e.preventDefault();
-    const book = new Book(
-      formTitle.value,
-      formAuthor.value,
-      formPages.value,
-      formReadStatus.checked,
-    );
-    books.push(book);
-    render();
-    modal.close();
-    form.reset();
-  };
-
-  const removeFromLibrary = (e) => {
-    const target = e.target.parentNode.dataset.title;
-    const index = books.findIndex((x) => x.title === target);
-    books.splice(index, 1);
-    render();
-  };
-
-  const toggleReadStatus = (e) => {
-    const target = e.target.parentNode.parentNode.parentNode.dataset.title;
-    const index = books.findIndex((x) => x.title === target);
-    books[index].readStatus = !books[index].readStatus;
-  };
-
   // bindEventsStatic
   showModal.addEventListener('click', () => {
     modal.showModal();
@@ -80,17 +87,35 @@ const library = (function () {
     modal.close();
     form.reset();
   });
-  form.addEventListener('submit', addToLibrary);
+  form.addEventListener('submit', addBookToShelf);
 
   const bindEventsDynamic = () => {
     booksContainer.querySelectorAll('.remove').forEach((btn) => {
-      btn.addEventListener('click', removeFromLibrary);
+      btn.addEventListener('click', removeBookFromShelf);
     });
+
     booksContainer.querySelectorAll('.toggle-switch span').forEach((toggle) => {
       toggle.addEventListener('click', toggleReadStatus);
     });
   };
-  return {
-    books,
+
+  const render = () => {
+    const finalMarkupArray = bookshelf
+      .map(({
+        title, author, pages, readStatus,
+      }) => template
+        .replaceAll('{title}', title)
+        .replace('{author}', author)
+        .replace('{pages}', pages)
+        .replace('{readStatus}', () => {
+          return readStatus === true ? 'checked' : '';
+        }));
+
+    const finalMarkup = finalMarkupArray.join('');
+    booksContainer.innerHTML = finalMarkup;
+
+    bindEventsDynamic();
   };
+
+  return { render };
 }());
